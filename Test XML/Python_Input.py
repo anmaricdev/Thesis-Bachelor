334fa079006c50f81e8d5eb1af131a6f435cc37f
Ante Maric <antemaric99@hotmail.com>
2025-06-01 23:07:15 +0200

    added Codeblock feature
    added an html codeblock toggle button for display purposes of the algorithm code to help in the exercises

---
+++ Test XML/Python_Input.py
@@ -0,0 +1,29 @@
+def bin_packing_worst_fit_var_capa(bin_capacities, elements):
+    is_packing_possible = True
+    max_capacity = max(bin_capacities)
+    m = len(bin_capacities)
+    n = len(elements)
+    bins = [[] for _ in range(m)]
+    bins_in_use = [i for i in range(m)]
+    bins_packed_after_failure = [[] for _ in range(m)]
+    for elem in elements:
+        # search for bin in use where it fits worst
+        worst_bin = -1
+        worst_remaining_capacity = -1
+        for i in range(len(bins_in_use)):
+            remaining_capacity = bin_capacities[bins_in_use[i]
+                                                ] - (sum(bins[bins_in_use[i]]) + elem)
+            if remaining_capacity >= 0 and remaining_capacity > worst_remaining_capacity:
+                worst_bin = bins_in_use[i]
+                worst_remaining_capacity = remaining_capacity
+        # found?
+        if worst_bin != -1:
+            bins[worst_bin] += [elem]
+            if not is_packing_possible:
+                bins_packed_after_failure[worst_bin] += [elem]
+            # is this bin full now?
+            if worst_remaining_capacity == 0:
+                bins_in_use.remove(worst_bin)
+        else:
+            is_packing_possible = False
+    return is_packing_possible, bins, bins_packed_after_failure
